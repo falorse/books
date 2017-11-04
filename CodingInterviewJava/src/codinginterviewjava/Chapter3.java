c1fd210c9c9eb955198048a950edf65301603b9b
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2017-11-04 20:20:49 +0900

    3-1 ongoing

--- CodingInterviewJava/src/codinginterviewjava/Chapter3.java
+++ CodingInterviewJava/src/codinginterviewjava/Chapter3.java
@@ -13,6 +13,7 @@ import java.lang.Exception;
  * @author fukuda
  */
 class Stack {
+
     Node top_;
 
     Object pop() {
@@ -31,58 +32,65 @@ class Stack {
     }
 
     Object peek() {
-        if(top_ == null) return null;
+        if (top_ == null) {
+            return null;
+        }
         return top_.data_;
     }
-    
-    String showStack(Node n){
-        if(n == null) n = top_;
-        if(top_ == null) return "";
-        
-        if(n.next_ != null){
+
+    String showStack(Node n) {
+        if (n == null) {
+            n = top_;
+        }
+        if (top_ == null) {
+            return "";
+        }
+
+        if (n.next_ != null) {
             return showStack(n.next_) + String.valueOf(n.data_);
-        }else{
-            return String.valueOf(n.data_); 
+        } else {
+            return String.valueOf(n.data_);
         }
     }
 }
 
-class Queue{
+class Queue {
+
     Node first_, last_;
-    
-    void enqueue(Object item){
-        if(first_ == null){
+
+    void enqueue(Object item) {
+        if (first_ == null) {
             last_ = new Node(item);
             first_ = last_;
-        }else{
+        } else {
             last_.next_ = new Node(item);
             last_ = last_.next_;
         }
     }
-    
-    void enqueue(Node item){
-        if(first_ == null){
+
+    void enqueue(Node item) {
+        if (first_ == null) {
             last_ = item;
             first_ = last_;
-        }else{
+        } else {
             last_.next_ = item;
             last_ = last_.next_;
         }
     }
-    
-    Object dequeue(){
-        if(first_ != null){
+
+    Object dequeue() {
+        if (first_ != null) {
             Object item = first_.data_;
             first_ = first_.next_;
             return item;
         }
         return null;
     }
-    
-    String showQueue(){
+
+    String showQueue() {
         String r = "";
         Node n = first_;
-        while(n != null){
+        while (n != null) {
             r += String.valueOf(n.data_);
             n = n.next_;
         }
@@ -90,10 +98,85 @@ class Queue{
     }
 }
 
+class ThreeStacks {
+
+    int[] topNums_;
+    int[] stackSizes_;
+    int[] stackPiles_;
+    int size_;
+    Node[] nodes_;
+
+    public ThreeStacks(int size) {
+        size_ = size > 96 ? size : 96;
+        topNums_ = new int[3];
+        stackSizes_ = new int[3];
+        stackPiles_ = new int[3];
+        for (int i = 0; i < 3; i++) {
+            topNums_[i] = i * 4;
+            stackSizes_[i] = 4;
+            stackPiles_[i] = 0;
+        }
+        nodes_ = new Node[size_];
+    }
+
+    void push(Object item, int num) throws Exception {
+        boolean canPush = true;
+        if (stackSizes_[num] == stackPiles_[num]) {
+            canPush = resize(num);
+        }
+        if (canPush) {
+            nodes_[topNums_[num]] = new Node(item);
+            stackPiles_[num]++;
+            topNums_[num]++;
+        } else {
+            throw new Exception("out of size");
+        }
+    }
+
+    //リサイズできたらtrueを返す
+    boolean resize(int num) {
+        int newSize = stackSizes_[num] * 2;
+        int allSize = 0;
+        for (int i = 0; i < 3; i++) {
+            if (i == num) {
+                allSize += newSize;
+                continue;
+            }
+            allSize += stackSizes_[i];
+        }
+        //リサイズするための容量がない場合
+        if (allSize > size_) {
+            return false;
+        } else {
+            //ある場合
+            switch (num) {
+                case 0:
+                    for(int i = size_ - 1; i > newSize - 1; i--){
+                        nodes_[i] = nodes_[i - stackSizes_[0]];
+                    }
+                    topNums_[1] += stackSizes_[0];
+                    topNums_[2] += stackSizes_[0];
+                    stackSizes_[0] *= 2;
+                    break;
+                case 1:
+                    for(int i = size_ - 1; i > newSize + stackSizes_[0] - 1 ; i--){
+                        nodes_[i] = nodes_[i - stackSizes_[1]];
+                    }
+                    topNums_[2] += stackSizes_[1];
+                    stackSizes_[1] *= 2;
+                    break;
+                default:
+                    stackSizes_[2] *= 2;
+                    break;
+            }
+        }
+        return true;
+    }
+}
+
 public class Chapter3 {
-    String m = "nn";
 
     public Chapter3() {
-        
     }
+
 }
--- CodingInterviewJava/test/codinginterviewjava/Chapter3Test.java
+++ CodingInterviewJava/test/codinginterviewjava/Chapter3Test.java
@@ -18,30 +18,29 @@ import static org.junit.Assert.*;
  */
 public class Chapter3Test {
     
-    static Chapter3 ch3_;
+    Chapter3 ch3_;
+    ThreeStacks ts_;
     
     public Chapter3Test() {
     }
     
     @BeforeClass
     public static void setUpClass() {
-        System.out.println("set up class");
-        ch3_ = new Chapter3();
     }
     
     @AfterClass
     public static void tearDownClass() {
-        System.out.println("tear down class");
     }
     
     @Before
     public void setUp() {
         System.out.println("set up");
+        ch3_ = new Chapter3();
+        ts_ = new ThreeStacks(96);
     }
     
     @After
     public void tearDown() {
-        System.out.println("tear down");
     }
 
     @Test
@@ -49,4 +48,60 @@ public class Chapter3Test {
         System.out.println("test");
     }
     
+    @Test
+    public void testResize(){
+        System.out.println("resize");
+        
+        assertEquals(true, ts_.resize(0));
+        assertEquals(8, ts_.stackSizes_[0]);
+        
+        ts_.resize(2);
+        assertEquals(8, ts_.stackSizes_[2]);
+        assertEquals(4, ts_.stackSizes_[1]);
+        
+        ts_.resize(0);
+        ts_.resize(0);
+        
+        assertEquals(32, ts_.stackSizes_[0]);
+        
+        ts_.resize(0);
+        assertEquals(false, ts_.resize(0));
+    }
+    
+    @Test
+    public void testPush() throws Exception{
+        System.out.println("push");
+        
+        ts_.push(1, 0);
+        ts_.push(2, 0);
+        
+        assertEquals(2, ts_.nodes_[ts_.topNums_[0] - 1].data_);
+        
+        ts_.push(3, 1);
+        
+        assertEquals(3, ts_.nodes_[ts_.topNums_[1] - 1].data_);
+        assertEquals(3, ts_.nodes_[4].data_);
+        
+        ts_.push(4, 0);
+        ts_.push(5, 0);
+        ts_.push(6, 2);
+        ts_.push(7, 0);
+        
+        assertEquals(7, ts_.nodes_[ts_.topNums_[0] - 1].data_);
+        assertEquals(3, ts_.nodes_[ts_.topNums_[1] - 1].data_);
+        assertEquals(6, ts_.nodes_[ts_.topNums_[2] - 1].data_);
+        
+        assertEquals(3, ts_.nodes_[8].data_);
+        assertEquals(6, ts_.nodes_[12].data_);
+        
+        ts_.push(8, 1);
+        ts_.push(9, 1);
+        ts_.push(10, 1);
+        ts_.push(11, 1);
+        
+        assertEquals(11, ts_.nodes_[ts_.topNums_[1] - 1].data_ );
+        assertEquals(6, ts_.nodes_[ts_.topNums_[2] - 1].data_);
+        assertEquals(6, ts_.nodes_[16].data_);
+        assertEquals(7, ts_.nodes_[ts_.topNums_[0] - 1].data_);
+    }
 }
