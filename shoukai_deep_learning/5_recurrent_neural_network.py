7f15d761f6a70485f839d9cd578db675a761291f
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2018-07-16 12:33:42 +0900

    fs

--- shoukai_deep_learning/5_recurrent_neural_network.py
+++ shoukai_deep_learning/5_recurrent_neural_network.py
@@ -10,7 +10,7 @@ tf.set_random_seed(1234)
 
 def inference(x, n_batch, maxlen=None, n_hidden=None, n_out=None):
     def weight_variable(shape):
-        initial = tf.truncated_normal(shape, stddev = 0.01)
+        initial = tf.truncated_normal(shape, stddev=0.01)
         return tf.Variable(initial)
 
     def bias_variable(shape):
@@ -21,10 +21,7 @@ def inference(x, n_batch, maxlen=None, n_hidden=None, n_out=None):
     initial_state = cell.zero_state(n_batch, tf.float32)
 
     state = initial_state
-
-    # 過去の隠れ層の出力を保存しておく
-    outputs = []
-
+    outputs = [] # 過去の隠れ層の出力を保存
     # 変数に対して共用の名前を付けておく。
     with tf.variable_scope('RNN'):
         for t in range(maxlen):
@@ -32,14 +29,14 @@ def inference(x, n_batch, maxlen=None, n_hidden=None, n_out=None):
             if t > 0:
                 tf.get_variable_scope().reuse_variables()
             # print('t:',t, " state,cell_out, x[:, t,:]",state,"\n", cell_output, '\n', x[:,t,:])
-            (cell_output, state) = cell(x[:,t,:] , state)
+            (cell_output, state) = cell(x[:, t, :] , state)
             outputs.append(cell_output)
 
     output = outputs[-1]
 
-    v = weight_variable([n_hidden, n_out])
+    V = weight_variable([n_hidden, n_out])
     c = bias_variable([n_out])
-    y = tf.matmul(output, v) + c
+    y = tf.matmul(output, V) + c  # 線形活性
 
     return y
 
@@ -55,13 +52,34 @@ def training(loss):
     train_step = optimizer.minimize(loss)
     return train_step
 
+class EarlyStopping():
+    def __init__(self, patience=0, verbose=0):
+        self._step = 0
+        self._loss = float('inf')
+        self.patience = patience
+        self.verbose = verbose
+
+    def validate(self, loss):
+        if self._loss < loss:
+            self._step += 1
+            if self._step > self.patience:
+                if self.verbose:
+                    print('early stopping')
+                return True
+        else:
+            self._step = 0
+            self._loss = loss
+
+        return False
+
+
 if __name__ == '__main__':
-    def sin(x, T = 100):
+    def sin(x, T=100):
         return np.sin(2.0 * np.pi * x / T)
 
-    def toy_problem(T = 100, ampl = 0.05):
+    def toy_problem(T=100, ampl=0.05):
         x = np.arange(0, 2 * T + 1)
-        noise = ampl * np.random.uniform(low = -0.1, high = 1.0, size = len(x))
+        noise = ampl * np.random.uniform(low=-1.0, high=1.0, size=len(x))
         return sin(x) + noise
 
     T = 100
@@ -83,12 +101,12 @@ if __name__ == '__main__':
     N_train = int(len(data) * 0.9)
     N_validation = len(data) - N_train
 
-    X_train, X_validation, Y_train, Y_validation =\
-      train_test_split(X, Y, test_size = N_validation)
+    X_train, X_validation, Y_train, Y_validation = \
+        train_test_split(X, Y, test_size=N_validation)
 
-    n_in = len(X[0][0])
+    n_in = len(X[0][0])  # 1
     n_hidden = 30
-    n_out = len(Y[0])
+    n_out = len(Y[0])  # 1
 
     x = tf.placeholder(tf.float32, shape=[None, maxlen, n_in])
     t = tf.placeholder(tf.float32, shape=[None, n_out])
@@ -97,6 +115,11 @@ if __name__ == '__main__':
     y = inference(x, n_batch, maxlen=maxlen, n_hidden=n_hidden, n_out=n_out)
     loss = loss(y, t)
     train_step = training(loss)
+
+    early_stopping = EarlyStopping(patience=10, verbose=1)
+    history = {
+        'val_loss': []
+    }
 
     epochs = 500
     batch_size = 10
@@ -109,15 +132,15 @@ if __name__ == '__main__':
 
     history = {'val_loss':[], 'val_acc':[]}
     for epoch in range(epochs):
-        x_, y_ = shuffle(X_train, Y_train)
+        X_, Y_ = shuffle(X_train, Y_train)
 
         for i in range(n_batches):
             start = i * batch_size
             end = start * batch_size
 
             sess.run(train_step, feed_dict={
-                    x: x_[start:end],
-                    t: y_[start:end],
+                    x: X_[start:end],
+                    t: Y_[start:end],
                     n_batch: batch_size
             })
 
@@ -130,5 +153,5 @@ if __name__ == '__main__':
         history['val_loss'].append(val_loss)
         print('epoch', epoch, ' validation loss', val_loss)
 
-        # if early_stopping.validate(val_loss):
-        #     break
+        if early_stopping.validate(val_loss):
+            break
